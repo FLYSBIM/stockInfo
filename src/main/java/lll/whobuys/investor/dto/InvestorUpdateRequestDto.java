60b1cc3dbe6f682724546f18f38b6514f12ee845
FLYSBIM <jj2358971@gmail.com>
2025-09-03 16:24:26 +0900

    [#4] - feat Investor dto 보완

--- src/main/java/lll/whobuys/investor/dto/InvestorUpdateRequestDto.java
+++ src/main/java/lll/whobuys/investor/dto/InvestorUpdateRequestDto.java
@@ -1,4 +1,35 @@
 package lll.whobuys.investor.dto;
 
-public class InvestorUpdateRequestDto {
+import lll.whobuys.investor.domain.InvestorType;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InvestorUpdateRequestDto implements InvestorRequestDto{
+
+    private Long investorId;
+    private InvestorUpdateData investorUpdateData;
+
+    public InvestorUpdateRequestDto(final Long investorId,final InvestorUpdateData investorUpdateData) {
+        this.investorId = investorId;
+        this.investorUpdateData = investorUpdateData;
+    }
+
+    public Long getInvestorId() {
+        return investorId;
+    }
+
+    public InvestorUpdateData getInvestorUpdateData() {
+        return investorUpdateData;
+    }
+
+    @Override
+    public String getName() {
+        return investorUpdateData.getUpdateName();
+    }
+
+    @Override
+    public InvestorType getInvestorType() {
+        return investorUpdateData.getUpdateType();
+    }
 }
