a0626b8e6aad3d96862dc6902d8c9e9f610a8d36
FLYSBIM <jj2358971@gmail.com>
2025-09-03 11:46:33 +0900

    [#0] feat - common dto 구현

---
+++ src/main/java/lll/whobuys/common/dto/ResponseDtoMethod.java
@@ -0,0 +1,9 @@
+package lll.whobuys.common.dto;
+
+public class ResponseDtoMethod {
+    public static String GET = "GET";
+    public static String POST = "POST";
+    public static String PATCH = "PATCH";
+    public static String PUT = "PUT";
+    public static String DELETE = "DELETE";
+}
