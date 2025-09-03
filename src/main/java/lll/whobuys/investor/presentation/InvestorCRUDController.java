6783428e8d8ba8aa5df0fea4b75495be0caf80b6
FLYSBIM <jj2358971@gmail.com>
2025-09-03 16:30:19 +0900

    [#4] feat - Investor 컨트롤러 구현

---
+++ src/main/java/lll/whobuys/investor/presentation/InvestorCRUDController.java
@@ -0,0 +1,40 @@
+package lll.whobuys.investor.presentation;
+
+import lll.whobuys.investor.application.InvestorCRUDService;
+import lll.whobuys.investor.dto.InvestorCreateRequestDto;
+import lll.whobuys.investor.dto.InvestorUpdateRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class InvestorCRUDController {
+
+    private final InvestorCRUDService investorCRUDService;
+
+    @Autowired
+    public InvestorCRUDController(InvestorCRUDService investorCRUDService) {
+        this.investorCRUDService = investorCRUDService;
+    }
+
+    @PostMapping("/investors")
+    public ResponseEntity createInvestor(@RequestBody final InvestorCreateRequestDto requestDto) {
+        return ResponseEntity.ok(investorCRUDService.createInvestor(requestDto));
+    }
+
+    @GetMapping("/investors/{investorId}")
+    public ResponseEntity getInvestor(@PathVariable final Long investorId) {
+        return ResponseEntity.ok(investorCRUDService.selectInvestor(investorId));
+    }
+
+    @PutMapping("/investors")
+    public ResponseEntity updateInvestor(@RequestBody final InvestorUpdateRequestDto requestDto) {
+        return ResponseEntity.ok(investorCRUDService.updateInvestor(requestDto));
+    }
+
+    @DeleteMapping("investors")
+    public ResponseEntity deleteInvestor(@RequestParam final Long investorId) {
+        return ResponseEntity.ok(investorCRUDService.deleteInvestor(investorId));
+    }
+}
